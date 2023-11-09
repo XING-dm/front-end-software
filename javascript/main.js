a3c7046b7630a8c16286fcaa4ce3324974677d82
XING-dm <xingdaiyamondo@gmail.com>
2023-11-09 16:46:03 +0800

    3,4

--- javascript/main.js
+++ javascript/main.js
@@ -20,43 +20,105 @@
 
 // alert("");
 
-let Choice = prompt("sholteai ustai hool ideh uu: 1. tiim  2.ugui");
 
-if(Choice == 1){
-    husel = prompt("tsai avah uu: 1. tiim  2.ugui");
-    if(husel == 1){
-        S = prompt("bansgtai yu: 1.tiim 2. ugui");
-        if(S == 1)
-            alert("banshtai tsai");
-        if(S == 2)
-            alert("budaatai tsai");
-    }
-    if(husel == 2){
-        Songolt = prompt("yutai shol ideh ve: 1.guril 2.puntuuz 3.goimon 4.bansh 5.mah 6.nogootoi");
-        if(Songolt == 1)
-            alert("lavsha");
-        if(Songolt == 2)
-            alert("huitsai");
-        if(Songolt == 3)
-            alert("goimontoi shol");
-        if(Songolt == 4)
-            alert("banshtai shol");
-        if(Songolt == 5)
-            alert("har shol");
-        if(Songolt == 6)
-            alert("nogootoi shol");
-    }
-}
-if (Choice == 2){
-    m = prompt("mah in tatsan uuu: 1.tiim 2.ugui");
-    if(m == 1){
-        Songolt = prompt("Guriltai yu budaatai yu: 1.tiim, 2.ugui");
-        if(Songolt == 1)
-            alert("lavsha");
-        if(Songolt == 2)
-            alert("huitsai");
-    }
-    if(m == 2){
-        alert("huitsai");
+
+
+// let Choice = prompt("sholteai ustai hool ideh uu: 1. tiim  2.ugui");
+
+// if(Choice == 1){
+//     husel = prompt("tsai avah uu: 1. tiim  2.ugui");
+//     if(husel == 1){
+//         S = prompt("bansgtai yu: 1.tiim 2. ugui");
+//         if(S == 1)
+//             alert("banshtai tsai");
+//         if(S == 2)
+//             alert("budaatai tsai");
+//     }
+//     if(husel == 2){
+//         Songolt = prompt("yutai shol ideh ve: 1.guril 2.puntuuz 3.goimon 4.bansh 5.mah 6.nogootoi");
+//         if(Songolt == 1)
+//             alert("lavsha");
+//         if(Songolt == 2)
+//             alert("huitsai");
+//         if(Songolt == 3)
+//             alert("goimontoi shol");
+//         if(Songolt == 4)
+//             alert("banshtai shol");
+//         if(Songolt == 5)
+//             alert("har shol");
+//         if(Songolt == 6)
+//             alert("nogootoi shol");
+//     }
+// }
+// if (Choice == 2){
+//     m = prompt("mah in tatsan uuu: 1.tiim 2.ugui");
+//     if(m == 1){
+//         g = prompt("Guriltai yu budaatai yu: 1.tiim, 2.ugui");
+//         if(g == 1){
+//             y = prompt("yaj bolgoson be: 1.sharsan 2.jignesen");
+//             if(y == 1){
+//                 hoo = prompt("hoolgogch hiih uu ");
+//                 if(hoo == 1){
+//                     alert("piroshki");
+//                 }
+//                 if(hoo == 2){
+//                     alert("huushuur");
+//                 }
+//             }
+//             if(y == 2){
+//                 hoos = prompt("hoolgogch hiih uu ");
+//                 if(hoos == 1){
+//                     alert("mantuun buuz");
+//                 }
+//                 if(hoos == 2){
+//                     alert("buuz");
+//                 }
+//             }
+//         }
+//         if(g == 2){
+//             o = promp("ondogtei yu: 1.tiim 2.no")
+//             if(o ==1){
+//                 alert("ondogtei bishtegs");
+//             }
+//             if(o ==2){
+//                 alert("teptels");
+//             }
+//         }
+//     }
+//     if(m == 2){
+//         yu = prompt("yutai holih ve: 1.guril 2.ondog 3.budaa 4.nogoo");
+//         if(yu == 1)
+//             alert("tsuivan");
+//         if(yu == 2)
+//             alert("ondogtei huurga");
+//         if(yu == 3){
+//             hii = prompt("hoorond in holih uu: 1.tiim 2.no");
+//             if(hii ==1){
+//                 alert("budaatai huurga");
+//             }
+//             if(hii ==2){
+//                 alert("guliash");
+//             }
+//         }
+//         if(yu == 4)
+//             alert("nogootoi huurga");
+//     }
+// }
+
+
+
+let number = prompt("ta toogoo oruulna uu: ");
+
+if(number % 2 ==0){
+   if(number % 3 ==0){
+        alert("3 t huvaagddag too tegsh too ")
+   }else{
+    alert("3 huvaagddaggui tegsh too ")
+   }
+} else if(number % 2==1){
+    if(number % 3 ==1){
+        alert("3 t huvaagddag too sondgoi too ")
+    }else if(number % 4 ==0){
+            alert("4t huvaagddahg tegsh too ")
     }
 }
